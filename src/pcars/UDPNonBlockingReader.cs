87c19f48584bdcf632eeb32461a3789875d3e8a9
Leroy McLean <leroythelegend@gmail.com>
2018-07-27 18:10:54 +1000

    Trying to fix format again

--- src/pcars/UDPNonBlockingReader.cs
+++ src/pcars/UDPNonBlockingReader.cs
@@ -4,24 +4,24 @@ using System.Net.Sockets;
 
 namespace pcars
 {
-    public class UDPNonBlockingReader : IReader
+public class UDPNonBlockingReader : IReader
     {
         readonly UdpClient client;
         IPEndPoint endPoint;
 
-		public UDPNonBlockingReader(int port)
+        public UDPNonBlockingReader(int port)
         {
             client = new UdpClient(port);
-			endPoint = new IPEndPoint(IPAddress.Any, 0);
+            endPoint = new IPEndPoint(IPAddress.Any, 0);
         }
 
         public Byte[] Read()
-		{
-		    if (client.Available != 0)
+        {
+            if (client.Available != 0)
             {
-				return client.Receive(ref endPoint);
-		    }
-			return new Byte[0];
+                return client.Receive(ref endPoint);
+            }
+            return new Byte[0];
         }
     }
 }
