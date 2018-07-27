4c50c6d8f4b1b00200cc81ece71f42a740c087f4
Leroy McLean <leroythelegend@gmail.com>
2018-07-27 17:20:56 +1000

    Added blocking udp reader

---
+++ src/pcars/IReader.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace pcars
+{
+    public interface IReader
+    {
+        Byte[] Read();
+    }
+}
---
+++ src/pcars/UDPBlockingReader.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace pcars
+{
+    public class UDPBlockingReader : IReader
+    {
+        readonly UdpClient client;
+        IPEndPoint endPoint;
+
+        public UDPBlockingReader(int port)
+        {
+            client = new UdpClient(port);
+            endPoint = new IPEndPoint(IPAddress.Any, 0);
+        }
+
+        public Byte[] Read()
+        {
+            return client.Receive(ref endPoint);
+        }
+    }
+}
